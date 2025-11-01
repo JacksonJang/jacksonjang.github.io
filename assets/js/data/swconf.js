4aa3d38ee3eb24338a340ca9e7b987a0360d3102
JacksonJang <JacksonJang@users.noreply.github.com>
2025-11-01 08:42:36 +0000

    deploy: 705668e2eaf3ff27930897226e89998daa0c7bc3

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761982561',resources: [
+    cacheName: 'chirpy-1761986553',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
